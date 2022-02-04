7239778767644a0cfe9300e5903ae333f6d1f058
caleb <caleb.miles33@gmail.com>
2022-02-04 11:42:08 -0600

    Creating New App

---
+++ artgallery/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class ArtPost(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
